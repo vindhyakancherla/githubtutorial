9dbbdcdb980a7fa20a3b2c0ad34102606e1e9dc3
vindhya <kancherla.vindhya09@gmail.com>
2021-11-11 23:12:55 +0530

    addition method is added to simpleprgm.java

--- simpleprgm.java
+++ simpleprgm.java
@@ -1,5 +1,10 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello git");  
-    }  
+    int add(int a,int b)
+    {
+
+    return int a+int b;
+
+    }{}  
 }  
